03221dc4ab09542db73260d483d20c4988aea4a1
sinnyuki <hmz1969a@gmail.com>
2023-06-03 16:40:34 +0800

    back_yukiBP_0.0.9
    yukiBP_0.0.9的mcshr_rwddm也会出现找不到解
    说明不论在wdm内使用group还是在进入wdm前就分组，都会出现找不到解的情况。
    
    来找找，到底是哪个被试让这个状况出现的

--- 1_Protocol/1_2_Planned_Analysis/1_2_3_R_package/batch/R/nmshr_rwddm.R
+++ 1_Protocol/1_2_Planned_Analysis/1_2_3_R_package/batch/R/nmshr_rwddm.R
@@ -10,101 +10,53 @@
 nmshr_rwddm <- function(list, Target, Paper_ID) {
   values <- data.frame()
   for(j in 1:length(list)) {
-
+    
 ################################################################################
-
+    
     SPE_half_1 <- list[[j]][[1]] %>%
       # RWiener::wdm 只识别lower upper
       dplyr::mutate(ACC = case_when(ACC == 0 ~ "lower",
                                     ACC == 1 ~ "upper")) %>%
-      # RWiener::wdm 只接受一个分组变量
-      split(.$Subject) %>%
+      # wdm每次只接受一个实验条件
+      dplyr::group_by(Subject, Matching, Identity, Session) %>%
+      dplyr::group_split() %>%
       # 执行wdm，然后将分割的结果重新组合
-      base::lapply(., function(df) {
-        tryCatch(
-          {df <- df %>%
-            tidyr::unite(group, Subject, Matching, Identity, Session, sep = "_") %>%
-            dplyr::mutate(group = factor(group))
-          ###############################Core Codes#################################
-          temp <- RWiener::wdm(df, xvar = "group", yvar = c("RT_sec", "ACC"))
-          ###############################Core Codes#################################
-          result <- data.frame(temp$coefficients) %>%
-            dplyr::mutate(group = rownames(.)) %>%
-            tidyr::separate(group, into = c("Subject", "Matching", "Identity", "Session"), sep = "_") %>%
-            tidyr::separate(Session, into = c("Session", "Indice"), sep = ":") %>%
-            tidyr::pivot_wider(names_from = Indice,
-                               values_from = temp.coefficients) %>%
-            dplyr::mutate(a = alpha, t = tau, z = beta, v = delta,
-                          Subject = as.numeric(Subject),
-                          Matching = factor(Matching,
-                                            levels = c("Matching", "Nonmatching")),
-                          Identity = factor(Identity,
-                                            levels = c("Self", "Friend", "Stranger")),
-                          Session = as.character(Session)
-            ) %>%
-            dplyr::arrange(Subject, Matching, Identity, Session) %>%
-            dplyr::select(Subject, Matching, Identity, Session, a, t, z, v)
-          return(result)
-          },
-          error = function(e) {
-            # 如果RWiener::wdm找不到解，则输出空值
-            result <- data.frame(Subject = NA, Matching = NA, Identity = NA, Session = NA,
-                                 a = NA, t = NA, z = NA, v = NA,
-                                 stringsAsFactors = FALSE  # 确保字符列不被转换为因子
-                                 )
-            return(result)  # 返回 NULL 或其他标识表示该元素出错
-          }
-        )
+      base::lapply(., function(df) { 
+        ###############################Core Codes#################################
+        temp <- RWiener::wdm(df, yvar = c("RT_sec", "ACC"))
+        ###############################Core Codes#################################
+        result <- data.frame(df$Subject[1], df$Matching[1], df$Identity[1], df$Session[1], 
+                             temp$coefficients[1], temp$coefficients[2], temp$coefficients[3], temp$coefficients[4])
+        colnames(result) <- c("Subject", "Matching", "Identity", "Session", 
+                              "a", "t", "z", "v")
+        rownames(result) <- NULL
+        return(result)
       }) %>%
       base::do.call(rbind, .)
-
+    
     SPE_half_2 <- list[[j]][[2]] %>%
       # RWiener::wdm 只识别lower upper
       dplyr::mutate(ACC = case_when(ACC == 0 ~ "lower",
                                     ACC == 1 ~ "upper")) %>%
-      # RWiener::wdm 只接受一个分组变量
-      split(.$Subject) %>%
+      # wdm每次只接受一个实验条件
+      dplyr::group_by(Subject, Matching, Identity, Session) %>%
+      dplyr::group_split() %>%
       # 执行wdm，然后将分割的结果重新组合
-      base::lapply(., function(df) {
-        tryCatch(
-          {df <- df %>%
-            tidyr::unite(group, Subject, Matching, Identity, Session, sep = "_") %>%
-            dplyr::mutate(group = factor(group))
-          ###############################Core Codes#################################
-          temp <- RWiener::wdm(df, xvar = "group", yvar = c("RT_sec", "ACC"))
-          ###############################Core Codes#################################
-          result <- data.frame(temp$coefficients) %>%
-            dplyr::mutate(group = rownames(.)) %>%
-            tidyr::separate(group, into = c("Subject", "Matching", "Identity", "Session"), sep = "_") %>%
-            tidyr::separate(Session, into = c("Session", "Indice"), sep = ":") %>%
-            tidyr::pivot_wider(names_from = Indice,
-                               values_from = temp.coefficients) %>%
-            dplyr::mutate(a = alpha, t = tau, z = beta, v = delta,
-                          Subject = as.numeric(Subject),
-                          Matching = factor(Matching,
-                                            levels = c("Matching", "Nonmatching")),
-                          Identity = factor(Identity,
-                                            levels = c("Self", "Friend", "Stranger")),
-                          Session = as.character(Session)
-            ) %>%
-            dplyr::arrange(Subject, Matching, Identity, Session) %>%
-            dplyr::select(Subject, Matching, Identity, Session, a, t, z, v)
-          return(result)
-          },
-          error = function(e) {
-            # 如果RWiener::wdm找不到解，则输出空值
-            result <- data.frame(Subject = NA, Matching = NA, Identity = NA, Session = NA,
-                                 a = NA, t = NA, z = NA, v = NA,
-                                 stringsAsFactors = FALSE  # 确保字符列不被转换为因子
-            )
-            return(result)  # 返回 NULL 或其他标识表示该元素出错
-          }
-        )
+      base::lapply(., function(df) { 
+        ###############################Core Codes#################################
+        temp <- RWiener::wdm(df, yvar = c("RT_sec", "ACC"))
+        ###############################Core Codes#################################
+        result <- data.frame(df$Subject[1], df$Matching[1], df$Identity[1], df$Session[1], 
+                             temp$coefficients[1], temp$coefficients[2], temp$coefficients[3], temp$coefficients[4])
+        colnames(result) <- c("Subject", "Matching", "Identity", "Session", 
+                              "a", "t", "z", "v")
+        rownames(result) <- NULL
+        return(result)
       }) %>%
       base::do.call(rbind, .)
-
-################################################################################
-
+    
+################################################################################ 
+    
     SPE_half_1_v <- SPE_half_1 %>%
       dplyr::select(Subject, Session, Matching, Identity, v) %>%
       dplyr::filter(Matching == "Matching") %>%
@@ -112,7 +64,7 @@ nmshr_rwddm <- function(list, Target, Paper_ID) {
                          values_from = v) %>%
       dplyr::mutate(v_SPE_1 = Self - !!sym(Target)) %>%
       dplyr::select(Subject, Session, v_SPE_1)
-
+    
     SPE_half_2_v <- SPE_half_2 %>%
       dplyr::select(Subject, Session, Matching, Identity, v) %>%
       dplyr::filter(Matching == "Matching") %>%
@@ -120,12 +72,12 @@ nmshr_rwddm <- function(list, Target, Paper_ID) {
                          values_from = v) %>%
       dplyr::mutate(v_SPE_2 = Self - !!sym(Target)) %>%
       dplyr::select(Subject, Session, v_SPE_2)
-
+    
     df_cor_v <- SPE_half_1_v %>%
       dplyr::left_join(SPE_half_2_v, by = c("Subject", "Session")) %>%
       dplyr::filter(!is.na(v_SPE_1) & !is.na(v_SPE_2)) %>%
       dplyr::filter(is.finite(v_SPE_1) & is.finite(v_SPE_2))
-
+    
     r_value_v <- cor(df_cor_v[,3], df_cor_v[,4], method = "pearson")
 
     values[j,1] <- "RW: v"
@@ -133,9 +85,9 @@ nmshr_rwddm <- function(list, Target, Paper_ID) {
     values[j,3] <- r_value_v[1]
     values[j,4] <- Target
     values[j,5] <- Paper_ID
-
-################################################################################
-
+    
+################################################################################     
+    
     SPE_half_1_z <- SPE_half_1 %>%
       dplyr::select(Subject, Session, Matching, Identity, z) %>%
       dplyr::filter(Matching == "Matching") %>%
@@ -143,7 +95,7 @@ nmshr_rwddm <- function(list, Target, Paper_ID) {
                          values_from = z) %>%
       dplyr::mutate(z_SPE_1 = Self - !!sym(Target)) %>%
       dplyr::select(Subject, Session, z_SPE_1)
-
+    
     SPE_half_2_z <- SPE_half_2 %>%
       dplyr::select(Subject, Session, Matching, Identity, z) %>%
       dplyr::filter(Matching == "Matching") %>%
@@ -151,22 +103,22 @@ nmshr_rwddm <- function(list, Target, Paper_ID) {
                          values_from = z) %>%
       dplyr::mutate(z_SPE_2 = Self - !!sym(Target)) %>%
       dplyr::select(Subject, Session, z_SPE_2)
-
+    
     df_cor_z <- SPE_half_1_z %>%
       dplyr::left_join(SPE_half_2_z, by = c("Subject", "Session")) %>%
       dplyr::filter(!is.na(z_SPE_1) & !is.na(z_SPE_2)) %>%
       dplyr::filter(is.finite(z_SPE_1) & is.finite(z_SPE_2))
-
+    
     r_value_z <- cor(df_cor_z[,3], df_cor_z[,4], method = "pearson")
-
+    
     values[j+length(list),1] <- "RW: z"
     values[j+length(list),2] <- j
     values[j+length(list),3] <- r_value_z[1]
     values[j+length(list),4] <- Target
     values[j+length(list),5] <- Paper_ID
 
-################################################################################
-
+################################################################################       
+    
   }
   colnames(values) <- c("X1", "X2", "X3", "V4", "V5")
   return(values)
