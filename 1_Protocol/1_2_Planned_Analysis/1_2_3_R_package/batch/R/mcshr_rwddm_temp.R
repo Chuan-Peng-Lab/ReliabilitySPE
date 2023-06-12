c6bbfca9513fcfa7000d096c6fc8d16fe642c0c8
yuki-961004 <hmz1969a@gmail.com>
2023-06-12 17:07:51 +0800

    test & temp function: rwddm
    检测函数rwddm是否在删除了极端之后可以正常运行
    重写了test和temp，这两个rmd是用于把打包后的rwddm函数拆出来单独运行
    
    目前单独运行没有问题
    在循环中运行有问题

---
+++ 1_Protocol/1_2_Planned_Analysis/1_2_3_R_package/batch/R/mcshr_rwddm_temp.R
@@ -0,0 +1,106 @@
+#' Title
+#'
+#' @param list list
+#' @param Target target
+#' @param Paper_ID Paper_ID
+#'
+#' @return 结果
+#' @export 结果
+#'
+mcshr_rwddm <- function(list, Target, Paper_ID) {
+
+  r_values_v <- list()
+  r_values_z <- list()
+
+  for(j in 1:length(list)) {
+
+    ################################################################################
+
+    RW_list_1 <- split(shl_test[[j]][[1]], shl_test[[j]][[1]]$Subject)
+    result1 <- list()
+    for(i in 1:length(RW_list_1)) {
+      RW_list_1[[i]] <- RW_list_1[[i]] %>%
+        # RWiener::wdm 只识别lower upper
+        dplyr::mutate(ACC = case_when(ACC == 0 ~ "lower",
+                                      ACC == 1 ~ "upper"))
+      result1[[i]] <- rwddm(df = RW_list_1[[i]])
+    }
+    SPE_half_1 <- base::do.call(rbind, result1)
+
+
+    RW_list_2 <- split(shl_test[[j]][[1]], shl_test[[j]][[1]]$Subject)
+    result2 <- list()
+    for(i in 1:length(RW_list_2)) {
+      RW_list_2[[i]] <- RW_list_2[[i]] %>%
+        # RWiener::wdm 只识别lower upper
+        dplyr::mutate(ACC = case_when(ACC == 0 ~ "lower",
+                                      ACC == 1 ~ "upper"))
+      result2[[i]] <- rwddm(df = RW_list_2[[i]])
+    }
+    SPE_half_2 <- base::do.call(rbind, result2)
+
+    ################################################################################
+
+    SPE_half_1_v <- SPE_half_1 %>%
+      dplyr::select(Subject, Session, Matching, Identity, v) %>%
+      dplyr::filter(Matching == "Matching") %>%
+      tidyr::pivot_wider(names_from = Identity,
+                         values_from = v) %>%
+      dplyr::mutate(v_SPE_1 = Self - !!sym(Target)) %>%
+      dplyr::select(Subject, Session, v_SPE_1)
+
+    SPE_half_2_v <- SPE_half_2 %>%
+      dplyr::select(Subject, Session, Matching, Identity, v) %>%
+      dplyr::filter(Matching == "Matching") %>%
+      tidyr::pivot_wider(names_from = Identity,
+                         values_from = v) %>%
+      dplyr::mutate(v_SPE_2 = Self - !!sym(Target)) %>%
+      dplyr::select(Subject, Session, v_SPE_2)
+
+    df_cor_v <- SPE_half_1_v %>%
+      dplyr::left_join(SPE_half_2_v, by = c("Subject", "Session")) %>%
+      dplyr::filter(!is.na(v_SPE_1) & !is.na(v_SPE_2)) %>%
+      dplyr::filter(is.finite(v_SPE_1) & is.finite(v_SPE_2))
+
+    r_values_v[j] <- cor(df_cor_v[,3], df_cor_v[,4], method = "pearson")
+
+    ################################################################################
+
+    SPE_half_1_z <- SPE_half_1 %>%
+      dplyr::select(Subject, Session, Matching, Identity, z) %>%
+      dplyr::filter(Matching == "Matching") %>%
+      tidyr::pivot_wider(names_from = Identity,
+                         values_from = z) %>%
+      dplyr::mutate(z_SPE_1 = Self - !!sym(Target)) %>%
+      dplyr::select(Subject, Session, z_SPE_1)
+
+    SPE_half_2_z <- SPE_half_2 %>%
+      dplyr::select(Subject, Session, Matching, Identity, z) %>%
+      dplyr::filter(Matching == "Matching") %>%
+      tidyr::pivot_wider(names_from = Identity,
+                         values_from = z) %>%
+      dplyr::mutate(z_SPE_2 = Self - !!sym(Target)) %>%
+      dplyr::select(Subject, Session, z_SPE_2)
+
+    df_cor_z <- SPE_half_1_z %>%
+      dplyr::left_join(SPE_half_2_z, by = c("Subject", "Session")) %>%
+      dplyr::filter(!is.na(z_SPE_1) & !is.na(z_SPE_2)) %>%
+      dplyr::filter(is.finite(z_SPE_1) & is.finite(z_SPE_2))
+
+    r_values_z[j] <- cor(df_cor_z[,3], df_cor_z[,4], method = "pearson")
+  }
+
+  # Calculate the mean of the Pearson correlation coefficients
+  r_values_vector_v <- unlist(r_values_v)
+  r_v <- mean(r_values_vector_v)
+  CI_v <- quantile(r_values_vector_v, c(0.025, 0.975))
+
+  r_values_vector_z <- unlist(r_values_z)
+  r_z <- mean(r_values_vector_z)
+  CI_z <- quantile(r_values_vector_z, c(0.025, 0.975))
+
+  values_v <- data.frame("Indice" = "RWDDM: v", "r" = r_v, "LLCI" = CI_v[1], "ULCI" = CI_v[2], "Target" = Target, "Paper_ID" = Paper_ID)
+  values_z <- data.frame("Indice" = "RWDDM: z", "r" = r_z, "LLCI" = CI_z[1], "ULCI" = CI_z[2], "Target" = Target, "Paper_ID" = Paper_ID)
+  values <- rbind(values_v, values_z)
+  return(values)
+}
